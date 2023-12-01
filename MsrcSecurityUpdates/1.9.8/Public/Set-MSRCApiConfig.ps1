12908b66bdbe2aacfa51604e681d171fbbc9ba9b
Emin <emin.atac@gmail.com>
2023-12-01 22:18:55 +0100

    Defaulting to API version 3.0

--- MsrcSecurityUpdates/1.9.8/Public/Set-MSRCApiConfig.ps1
+++ MsrcSecurityUpdates/1.9.8/Public/Set-MSRCApiConfig.ps1
@@ -8,17 +8,23 @@ Param(
     [ValidateNotNull()]
     [System.Management.Automation.PSCredential]
     [System.Management.Automation.Credential()]
-    $ProxyCredential = [System.Management.Automation.PSCredential]::Empty
+    $ProxyCredential = [System.Management.Automation.PSCredential]::Empty,
 
+    [Parameter()]
+    [ValidateSet('3.0','2.0')]
+    [System.String]$APIVersion='3.0'
 )
 Begin {}
 Process {
     if ($PSCmdlet.ShouldProcess('Set config for MSRC API')) {
         # We set required shared variables
-        $global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v2.0'
+        $global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v{0}' -f $APIVersion
         Write-Verbose -Message "Successfully defined a msrcApiUrl global variable that points to $($global:msrcApiUrl)"
 
-        $global:msrcApiVersion = 'api-version=2016-08-01'
+        $global:msrcApiVersion = Switch ($APIVersion) {
+         '2.0' { 'api-version=2016-08-01'}
+         '3.0' { 'api-version=2023-11-01'}
+        }
         Write-Verbose -Message "Successfully defined a msrcApiVersion global variable that points to $($global:msrcApiVersion)"
 
         if ($ProxyCredential -ne [System.Management.Automation.PSCredential]::Empty) {
